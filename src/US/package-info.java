ccadcd02ddec644df130ab8d1668a22a61b66b45
Kanchan Sanap <kanchan.sanap@globant.com>
2019-02-13 15:19:10 +0530

    mangal

---
+++ src/US/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author kanchan.sanap
+ *
+ */
+package US;
---
+++ src/US/santa_clara.java
@@ -0,0 +1,9 @@
+package US;
+
+public class santa_clara {
+
+	public static void main(String[] args) {
+		System.out.println("kanchan");
+	}
+
+}
